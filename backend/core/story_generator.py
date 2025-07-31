e019474de01f905d5a4f0212d80bacb4d6d676b3
Ahmed <aaa26770123@gmail.com>
2025-07-31 15:55:19 -0600

    Remove choreo managed secrets

--- backend/core/story_generator.py
+++ backend/core/story_generator.py
@@ -20,8 +20,8 @@ class StoryGenerator:
         openai_api_key = os.getenv("CHOREO_OPENAI_CONNECTION_OPENAI_API_KEY")
         service_url = os.getenv("CHOREO_OPENAI_CONNECTION_SERVICEURL")
 
-        if openai_api_key and service_url:
-            return ChatOpenAI(model=model, api_key=openai_api_key, base_url=service_url)
+        # if openai_api_key and service_url:
+        #     return ChatOpenAI(model=model, api_key=openai_api_key, base_url=service_url)
 
         return ChatOpenAI(model=model)
 
